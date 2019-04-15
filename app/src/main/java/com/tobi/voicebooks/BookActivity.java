653166cd214eb283a31476bebaacb10adafa3dba
tobi <tobi.akiny@gmail.com>
2019-04-15 21:03:58 +0100

    implemented Book activity, -- moved initialisation of DB to the oncreate method

--- app/src/main/java/com/tobi/voicebooks/BookActivity.java
+++ app/src/main/java/com/tobi/voicebooks/BookActivity.java
@@ -1,18 +1,22 @@
 package com.tobi.voicebooks;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
 
 import com.tobi.voicebooks.db.VoiceBooksDatabase;
+import com.tobi.voicebooks.db.entities.BookEntity;
 import com.tobi.voicebooks.views.TranscriptView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BookActivity extends AppCompatActivity {
+    public static final String ID_KEY = "id";
+
     private TextView title;
-    private VoiceBooksDatabase database = Utils.getDatabase(this);
-    private Repository repository = new Repository(database, this);
+    private VoiceBooksDatabase database;
+    private Repository repository;
     private TranscriptView content;
 
     @Override
@@ -24,6 +28,15 @@ public class BookActivity extends AppCompatActivity {
         content = findViewById(R.id.book_content);
 
         long bookId = 0;
+
+        // initialise database access
+        database = Utils.getDatabase(this);
+        repository = new Repository(database, this);
+
+        // get book ID from the bundle
+        final Bundle extras = getIntent().getExtras();
+
+        // setup title watcher
         repository.getBookTitle(bookId, newTitle -> {
             final String collectedTitle = Utils.formatWords(newTitle);
             title.setText(collectedTitle);
